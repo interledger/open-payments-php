f90638e61372965dea14b0a9b69b7fd40ffe9f93
Adrian Boros <adiboros@hotmail.com>
2024-11-18 13:44:54 +0200

    add http signature helper functions and tests

---
+++ tests/Http/Signature/CreateContentHeadersTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class CreateContentHeadersTest extends TestCase
+{
+    public function testCreateContentHeaders()
+    {
+        $body = '{"key":"value"}';
+
+        // Expected headers
+        $expectedHeaders = [
+            'Content-Digest' => 'sha-512=' . base64_encode(hash('sha512', $body, true)),
+            'Content-Length' => strlen($body),
+            'Content-Type' => 'application/json'
+        ];
+
+        // Call the function
+        $result = \OpenPayments\Utils\createContentHeaders($body);
+
+        // Assertions
+        $this->assertIsArray($result);
+        $this->assertEquals($expectedHeaders, $result);
+    }
+}
