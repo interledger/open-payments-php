252f8bb8b58a369d29d11f40bf1304befcdf9d1c
Adrian Boros <adiboros@hotmail.com>
2025-04-11 14:53:21 +0300

    remove open api config and dependency

--- lib/Config/OpenApi/schemas.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'openapi-file' => dirname(dirname(__DIR__)) .'/OpenApi/Specs/schemas.yaml',
-    'namespace' => 'OpenPayments\\OpenApi\\Generated\\Schemas',
-    'directory' => dirname(dirname(__DIR__)) . '/OpenApi/Generated/Schemas',
-    'date-format' => \DateTime::RFC3339,
-];
