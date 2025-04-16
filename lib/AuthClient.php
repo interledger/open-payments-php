96c63e63c89fd69f67b153f8aaa646f75feeb2c5
Adrian Boros <adiboros@hotmail.com>
2025-04-16 10:16:19 +0300

    remove dev dependency

--- lib/AuthClient.php
+++ lib/AuthClient.php
@@ -2,8 +2,6 @@
 
 namespace OpenPayments;
 
-require_once __DIR__.'/../vendor/autoload.php';
-
 use OpenPayments\Config\Config;
 use OpenPayments\Services\GrantService;
 use OpenPayments\Services\IncomingPaymentService;
