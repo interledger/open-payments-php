d2fa06dd12ebe963137f1d7631e13f601881c89e
Adrian Boros <adiboros@hotmail.com>
2026-06-05 21:49:38 +0300

    chore(specs): add SpecVersion constant tracking upstream spec version

---
+++ lib/OpenApi/SpecVersion.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenPayments\OpenApi;
+
+class SpecVersion
+{
+    /**
+     * The version of the open-payments-specifications this library was built against.
+     *
+     * @see https://github.com/interledger/open-payments-specifications
+     */
+    public const string SPEC_VERSION = '1.3.2';
+}
